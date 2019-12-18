a4e36ea81b161e7c560de99b67787d18487f3c50
yuhexin <yuhexin@git.com>
2019-12-18 21:19:33 +0800

    $ git commit -m

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/home'
+import Login from '../views/login'
 
 Vue.use(VueRouter)
 
-const routes = []
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/home', component: Home },
+    { path: '/login', component: Login }
+  ]
 })
-
-export default router
