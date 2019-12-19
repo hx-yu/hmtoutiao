592af2d226a52e2b17a30ecf54acf2d55530a665
yuhexin <yuhexin@git.com>
2019-12-20 00:05:40 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout_aside.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="layout_aside">
+        <div class="title">
+          <img src="../../assets/img/logo_admin.png" alt />
+        </div>
+        <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+          <el-menu-item>首页</el-menu-item>
+          <el-submenu index="1">
+            <template slot="title">内容管理</template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">粉丝管理</template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝图像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item>账户信息</el-menu-item>
+        </el-menu>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout_aside {
+  height: 100vh;
+  background-color: #323745;
+  .title {
+    text-align: center;
+    background-color: #2e2f32;
+    padding: 10px 0;
+    img {
+      height: 35px;
+    }
+  }
+  .el-menu {
+    border-right: none;
+  }
+}
+</style>
---
+++ src/components/home/layout_header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-row class="layout_header" type="flex" align="middle">
+      <el-col class="left" :span="12">
+        <i class="el-icon-s-fold"></i>
+        <span>江苏传智博客教育科技股份有限公司</span>
+      </el-col>
+      <el-col class="right" :span="12">
+        <el-row type="flex" justify="end" align="middle">
+          <img class="user_pic" src="../../assets/img/avatar.jpg" alt />
+          <el-dropdown>
+            <span class="el-dropdown-link">啥也不是</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item info="person">个人信息</el-dropdown-item>
+              <el-dropdown-item info="git">git地址</el-dropdown-item>
+              <el-dropdown-item info="logout">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout_header {
+  height: 60px;
+  .left {
+    line-height: 60px;
+    i {
+      font-size: 18px;
+    }
+    span {
+      font-size: 16px;
+      margin-left: 5px;
+    }
+  }
+  .right {
+    .user_pic {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+    }
+    .el-dropdown-link {
+      margin-left: 10px;
+      cursor: pointer;
+      color: #409eff;
+    }
+    .el-icon-arrow-down {
+      margin-right: 15px;
+      font-size: 12px;
+      color: #409eff;
+    }
+  }
+}
+</style>
