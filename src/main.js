3c773bd13b2d03f009dce633cc91737debfe1d9e
yuhexin <yuhexin@git.com>
2019-12-19 19:48:02 +0800

    黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.config.productionTip = false
 
 new Vue({
