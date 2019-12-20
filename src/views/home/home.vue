2ee87b0880d2575f0de8df9d3ab0bd9fcf2f09c9
yuhexin <yuhexin@git.com>
2019-12-20 10:21:51 +0800

    黑马头条PC-主页模块-默认导航

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,17 +1,37 @@
 <template>
 <div>
-    main区内容
+    <el-carousel :interval="4000" type="card" height="300px">
+    <el-carousel-item v-for="item in ImgURL" :key="item">
+        <img :src="item" alt="">
+    </el-carousel-item>
+  </el-carousel>
 </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      ImgURL: ['http://img5.imgtn.bdimg.com/it/u=3720661285,1522818110&fm=26&gp=0.jpg', 'http://img2.imgtn.bdimg.com/it/u=2171938763,1005411525&fm=26&gp=0.jpg', 'http://img1.imgtn.bdimg.com/it/u=482823002,2048659319&fm=26&gp=0.jpg', 'http://img2.imgtn.bdimg.com/it/u=1485165718,2220658900&fm=26&gp=0.jpg', 'http://img4.imgtn.bdimg.com/it/u=4115246281,1172513023&fm=26&gp=0.jpg']
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
 
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
