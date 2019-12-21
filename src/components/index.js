cf4a6a4aa679381b4279072c96fbed45144d196d
yuhexin <yuhexin@git.com>
2019-12-21 22:04:57 +0800

    黑马头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutHeader from './home/layout_header'
 import layoutAside from './home/layout_aside'
+import breadCrumb from './common/breadcrumb'
 export default {
   install (Vue) {
     Vue.component('layoutheader', layoutHeader)
     Vue.component('layoutaside', layoutAside)
+    Vue.component('breadcrumb', breadCrumb)
   }
 }
