d6ed758740909480e126f0c1bdf99f2f1dd48b50
yuhexin <yuhexin@git.com>
2019-12-22 23:44:21 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/materialmaster/index.vue
+++ src/views/materialmaster/index.vue
@@ -9,7 +9,11 @@
           <el-card v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
             <el-row type="flex" justify="space-around" align="middle">
-              <i @click="collectOrCancel(item.id)" :style="{color:item.is_collected?'red':'#ccc'}" class="el-icon-star-on"></i>
+              <i
+                @click="collectOrCancel(item.id)"
+                :style="{color:item.is_collected?'red':'#ccc'}"
+                class="el-icon-star-on"
+              ></i>
               <i class="el-icon-delete-solid"></i>
             </el-row>
           </el-card>
@@ -23,6 +27,9 @@
         </div>
       </el-tab-pane>
     </el-tabs>
+    <el-row style="height:60px;" type="flex" align="middle" justify="center">
+      <el-pagination background layout="prev, pager, next" :page-size="page.pageSize" :current-page="page.currentPage" :total="page.total" @current-change="changePage"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -31,15 +38,25 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        total: 0,
+        pageSize: 8,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     collectOrCancel (id) {
       alert(id)
     },
     // 点击tabs请求全部列表或收藏列表
     allOrCollect () {
+      this.page.currentPage = 1
       this.getMaterial()
     },
     // 请求数据
@@ -47,11 +64,14 @@ export default {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: this.activeName === 'collect' // 通过判断tabs名字查询是否为收藏的数据
+          collect: this.activeName === 'collect', // 通过判断tabs名字查询是否为收藏的数据
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
       }).then(result => {
         console.log(result)
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
