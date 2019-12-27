a0aa0c27d6aed56e9629e5478ce1d4e4ef5a5949
yuhexin <yuhexin@git.com>
2019-12-27 22:03:39 +0800

    黑马头条PC-账户信息-页面结构

--- src/views/userinfo/index.vue
+++ src/views/userinfo/index.vue
@@ -3,12 +3,48 @@
     <breadcrumb slot="header">
       <template slot="title">用户信息</template>
     </breadcrumb>
+    <el-form style="margin-left:100px;" label-width="100px">
+        <el-form-item label="用户名：" style="width:40%">
+            <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="简介：" style="width:40%">
+            <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱：" style="width:40%">
+            <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="手机号：" style="width:40%">
+            <el-input></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary">保存</el-button>
+        </el-form-item>
+    </el-form>
+    <el-upload class="uesrImg" action="" :http-request="uploadUserImg">
+        <img src="../../assets/img/avatar.jpg" alt="">
+    </el-upload>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      dataForm: {}
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+.uesrImg{
+    position: absolute;
+    top: 200px;
+    right: 200px;
+    img{
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+    }
+}
 </style>
