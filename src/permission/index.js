1fec793e5ea265bda006036a38acbe7d5009d7a3
yuhexin <yuhexin@git.com>
2019-12-28 23:42:29 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,10 @@
 // 引入路由
 import router from '../router'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 给路由配置导航守卫
 router.beforeEach((to, from, next) => {
+  nprogress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user_token')
     if (token) {
@@ -12,4 +15,7 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  nprogress.done()
 })
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
 import HomeMain from '../views/home/home.vue'
-
 Vue.use(VueRouter)
 
 export default new VueRouter({
