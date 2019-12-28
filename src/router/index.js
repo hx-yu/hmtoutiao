705bde9e67cfd87207a3b67aa182c31a0769188e
yuhexin <yuhexin@git.com>
2019-12-28 23:48:32 +0800

    黑马头条PC-404页面应用

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
+    { path: '*', component: () => import('../views/404') },
     { path: '/', redirect: '/login' },
     {
       path: '/home',
---
+++ src/views/404/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pic404">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.pic404{
+    height: 100vh;
+    background: url('../../assets/img/404.png');
+    background-size: cover;
+}
+</style>
